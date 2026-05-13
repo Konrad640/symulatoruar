e2d1b953642547127926204fba5950a303f73af3
KacBanasik <kb316308@student.polsl.pl>
2026-05-13 21:58:37 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,7 +17,6 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    arx({-0.4, 0.0, 0.0}, {0.6, 0.0, 0.0}, 1, 0.0)
     , arx({-0.4, 0.0, 0.0}, {0.6, 0.0, 0.0}, 1, 0.0)
     , pid(0.5, 5.0, 0.2)
     , gen(Generator::TypSygnalu::PROSTOKATNY, 0, 1, 10, 0.5, 1.0)
